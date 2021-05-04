a15f19f077a8f04c6502c145d85d4f18444ec072
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-05-05 01:16:26 +0530

    Delete app.tsx

--- screenshots/app.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-  import ReactDOM from 'react-dom'
-  import MUIBOTTOMNAVIGATION from './components/muiBottomNavigation'
-
-  ReactDOM.render(<MUIBOTTOMNAVIGATION/>,document.getElementById('root'))
