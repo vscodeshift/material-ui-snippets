726d9e3d7a6db97135890e9e5b87861a7d715957
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-05-05 15:39:10 +0530

    Updated CI

--- screenshots/index.ts
+++ screenshots/index.ts
@@ -1,36 +1,43 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable no-console */
 import { webkit } from 'playwright'
 import fs from 'fs'
 import { build } from 'esbuild'
 
+const capitalize = (s: string): string => s[0].toUpperCase() + s.substr(1)
+
 async function go() {
   const browser = await webkit.launch()
-  const files = fs.readdirSync('components/')
+  const files = fs.readdirSync('components/').map(e => e.split('.')[0])
   // const files = ['muiAppBar.tsx', 'muiRadioLabel.tsx']
 
-  for await (let file of files) {
-    file = file.split('.')[0]
-    const ComponentName = file.toUpperCase()
-    const App = `import React from 'react'
-  import ReactDOM from 'react-dom'
-  import ${ComponentName} from './components/${file}'
+  const App = `import React from 'react'
+import ReactDOM from 'react-dom'
+${files.map(e => `import ${capitalize(e)} from './components/${e}'`).join('\n')}
+import { Switch, HashRouter, Route } from 'react-router-dom'
 
-  ReactDOM.render(<${ComponentName}/>,document.getElementById('root'))`
+ReactDOM.render(<HashRouter>
+  <Switch>
+    ${files
+      .map(e => `<Route path="/${e}" component={${capitalize(e)}} />`)
+      .join('    \n')}
+  </Switch>
+</HashRouter>,document.getElementById('root'))`
 
-    const fileName = 'app.tsx'
-    fs.writeFile(fileName, App, e => e && console.log(e))
+  const fileName = 'app.tsx'
+  fs.writeFile(fileName, App, e => e && console.log(e))
 
-    await build({
-      entryPoints: [fileName],
-      treeShaking: true,
-      bundle: true,
-      outfile: 'app.js',
-    }).catch(() => process.exit(1))
+  await build({
+    entryPoints: [fileName],
+    treeShaking: true,
+    bundle: true,
+    outfile: 'app.js',
+  }).catch(() => process.exit(1))
 
+  for await (const file of files) {
     const page = await browser.newPage()
     await page.setViewportSize({ width: 512, height: 400 })
-    await page.goto(`file://${__dirname}/index.html`)
-    await page.waitForTimeout(1000)
+    await page.goto(`file://${__dirname}/index.html#/${file}`)
     await page.screenshot({ path: `img/${file}.png`, fullPage: true })
   }
   return browser.close()
