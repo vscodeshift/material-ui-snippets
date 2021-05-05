82c94f6c91ad475424a67368c6b01e608e672206
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-05-05 09:09:37 +0530

    Updated CI

--- screenshots/index.ts
+++ screenshots/index.ts
@@ -6,31 +6,32 @@ import { build } from 'esbuild'
 async function go() {
   const browser = await webkit.launch()
 
-  // fs.readdirSync('components/').forEach(async file => {
-  const file = 'muiBottomNavigation.tsx'.split('.')[0]
-  const ComponentName = file.toUpperCase()
-  const App = `import React from 'react'
+  fs.readdirSync('components/').forEach(async file => {
+    // const file = 'muiBottomNavigation.tsx'.split('.')[0]
+    file = file.split('.')[0]
+    const ComponentName = file.toUpperCase()
+    const App = `import React from 'react'
   import ReactDOM from 'react-dom'
   import ${ComponentName} from './components/${file}'
 
   ReactDOM.render(<${ComponentName}/>,document.getElementById('root'))`
 
-  const fileName = 'app.tsx'
-  fs.writeFile(fileName, App, e => e && console.log(e))
+    const fileName = 'app.tsx'
+    fs.writeFile(fileName, App, e => e && console.log(e))
 
-  await build({
-    entryPoints: [fileName],
-    treeShaking: true,
-    bundle: true,
-    outfile: 'app.js',
-  }).catch(() => process.exit(1))
+    await build({
+      entryPoints: [fileName],
+      treeShaking: true,
+      bundle: true,
+      outfile: 'app.js',
+    }).catch(() => process.exit(1))
 
-  const page = await browser.newPage()
-  await page.setViewportSize({ width: 512, height: 400 })
-  await page.goto(`file://${__dirname}/index.html`)
-  await page.waitForTimeout(1000)
-  await page.screenshot({ path: `img/${file}.png`, fullPage: true })
-  // })
+    const page = await browser.newPage()
+    await page.setViewportSize({ width: 512, height: 400 })
+    await page.goto(`file://${__dirname}/index.html`)
+    await page.waitForTimeout(1000)
+    await page.screenshot({ path: `img/${file}.png`, fullPage: true })
+  })
   return browser.close()
 }
 
